febf562b0da5fc021d44ea6d6cb17ec62b2068fa
Ayush Saxena <ayushsaxena823@gmail.com>
2022-10-25 20:03:20 +0530

    media query hook added

--- src/Components/Nbar.js
+++ src/Components/Nbar.js
@@ -2,29 +2,86 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "./../assets/Logo.png"
+import logo from "./../assets/Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-bootstrap";
 import { useLocation } from "react-router";
+import { useMediaQuery } from "@mui/material";
 
 const Nbar = () => {
   const location = useLocation();
+  const matches = useMediaQuery("(max-width:786px)");
   return (
+
     <div>
-      <Navbar collapseOnSelect bg="light" expand="lg" >
+      <Navbar collapseOnSelect bg="light" expand="lg">
         <Container className="nav">
-          <img src={logo} alt="Celestial Biscuit IGDTUW" className="logo"/>
-            <Nav className="navbox" variant="pills" activeKey={location.pathname}>
-              <NavLink href="/" className="link" >HOME</NavLink>
-              <NavLink href="/Project"  className="link" >PROJECTS</NavLink>
-              <NavLink href="/Gallery"  className="link" >GALLERY</NavLink>
-              <NavLink href="/About"  className="link" >ABOUT</NavLink>
-            </Nav>
-            <Nav className="NavSocials">
-              <div className="social"><a href="https://twitter.com/cbigdtuw"  target="_blank" rel="noopener noreferrer" > <FontAwesomeIcon icon="fa-brands fa-twitter" className="icon" /></a></div>
-              <div className="social"><a href="https://www.instagram.com/celestialbiscuit/"   target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon="fa-brands fa-instagram" className="icon"/></a></div>
-              <div className="social"><a href="https://www.linkedin.com/company/celestial-biscuit-igdtuw/"  target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon="fa-brands fa-linkedin" className="icon"/></a></div>  
-            </Nav>
+          <img src={logo} alt="Celestial Biscuit IGDTUW" className="logo" />
+          {matches ? (
+            <></>
+          ) : (
+            <>
+              <Nav
+                className="navbox"
+                variant="pills"
+                activeKey={location.pathname}
+              >
+                <NavLink href="/" className="link">
+                  HOME
+                </NavLink>
+                <NavLink href="/Project" className="link">
+                  PROJECTS
+                </NavLink>
+                <NavLink href="/Gallery" className="link">
+                  GALLERY
+                </NavLink>
+                <NavLink href="/About" className="link">
+                  ABOUT
+                </NavLink>
+              </Nav>
+              <Nav className="NavSocials">
+                <div className="social">
+                  <a
+                    href="https://twitter.com/cbigdtuw"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    <FontAwesomeIcon
+                      icon="fa-brands fa-twitter"
+                      className="icon"
+                    />
+                  </a>
+                </div>
+                <div className="social">
+                  <a
+                    href="https://www.instagram.com/celestialbiscuit/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    <FontAwesomeIcon
+                      icon="fa-brands fa-instagram"
+                      className="icon"
+                    />
+                  </a>
+                </div>
+                <div className="social">
+                  <a
+                    href="https://www.linkedin.com/company/celestial-biscuit-igdtuw/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {" "}
+                    <FontAwesomeIcon
+                      icon="fa-brands fa-linkedin"
+                      className="icon"
+                    />
+                  </a>
+                </div>
+              </Nav>
+            </>
+          )}
         </Container>
       </Navbar>
     </div>
