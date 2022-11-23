f91a1c0e7ca55e9773d340bbd509fe9296d35925
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2022-11-23 18:31:40 +0530

    Made some changes

--- src/Components/PhotoGrid.js
+++ src/Components/PhotoGrid.js
@@ -4,9 +4,10 @@ const PhotoGrid = () => {
   return (
     <div className='gallery-main'>
         <div className='gallery-container'>
-        <div className='head'><h3>BIG BANG</h3><div className='line'></div></div>
+        {/* <div className='head'><h3>BIG BANG</h3><div className='line'></div></div> */}
         <div className='container'>
-            <div className='row-main'>
+            <h1>COMING SOON....</h1>
+            {/* <div className='row-main'>
                 <div className='column-main'>
                     <div className='big-main'></div>
                 </div>
@@ -35,7 +36,7 @@ const PhotoGrid = () => {
                 <div className='column-main'>
                     <div className='big-main'></div>
                 </div>
-            </div>
+            </div> */}
         </div>
     </div>
     </div>
